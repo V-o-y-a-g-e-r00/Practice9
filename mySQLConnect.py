3f6af0b2c0c8f1e8c47fc47d2d5193c25004fd01
fmol00 <fmol00@mail.ru>
2022-07-07 18:22:23 +0300

    Продолжение работы над базой данных

--- mySQLConnect.py
+++ mySQLConnect.py
@@ -1,6 +1,10 @@
+
+
 def PutInmySQL(List):
 	from getpass import getpass
 	from mysql.connector import connect, Error
+
+	import re #Для работы со строками
 
 	try: #Все соединения с базой данных оборачивайте в блоки try ... except  .Так будет проще перехватить и изучить любые исключения.
 		fin= open('IdmySQL.txt', 'r')
@@ -13,24 +17,65 @@ def PutInmySQL(List):
 				#password=getpass("Пароль: "),
 				user=user,
 				password=password,
-				database="online_movie_rating",
+				#database="online_movie_rating",
 		) as connection:
 				print(connection)
 			
 				#Пошла работа с базами данных
-				create_db_query = "CREATE DATABASE online_movie_rating"
-				#with connection.cur>sor() as cursor:
-				#	cursor.execute(create_db_query)
-
+				#Проверяем, что таблица существует
+				print("Checking existence of TouristPlaces db...")
+				IsTableExist=0
 				show_db_query = "SHOW DATABASES"
 				with connection.cursor() as cursor:
 					cursor.execute(show_db_query)
 					for db in cursor:
-						print(db)
+						temp=re.split('\'', str(db))[1]
+						if temp=='TouristPlaces':
+							IsTableExist=1
+						#print(db)
+				#print('IsTableExist=', IsTableExist)
+
+				#Если базы данных нет, то создаем её и создаем в ней таблицу.
+				if IsTableExist==0:				
+					print("TouristPlaces db does not exist. Creating TouristPlaces db...")
+					create_db_query = "CREATE DATABASE IF NOT EXISTS TouristPlaces" 
+					with connection.cursor() as cursor:
+						cursor.execute(create_db_query)
+
+					use_db_query = "USE TouristPlaces"
+					with connection.cursor() as cursor:
+						cursor.execute(use_db_query)
+
+
+
+
+
+					create_Places_table_query = """
+					CREATE TABLE Places(
+						id INT AUTO_INCREMENT PRIMARY KEY,
+						name VARCHAR(2047),
+						tag VARCHAR(2047),
+						description VARCHAR(10239),
+						latitude DECIMAL(9,6),
+						longitude DECIMAL(9,6)
+					)
+					"""
+					with connection.cursor() as cursor:
+						cursor.execute(create_Places_table_query)
+						connection.commit()
+						print("TouristPlaces db has been created")
+				else:
+					print("TouristPlaces db exists.")
+
 
 	#ловим необработанные исключения при работе с соединением
 	except Error as e:
 		print(e)
 
 List=[]
+
 PutInmySQL(List)
+
+
+
+
