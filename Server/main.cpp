618760e36d8b3f9913d3b5322ed0ab160b6bb4e0
fmol00 <fmol00@mail.ru>
2022-07-14 15:42:51 +0300

    Запускется без ошибок, но не может подключиться к бд

--- Server/main.cpp
+++ Server/main.cpp
@@ -3,7 +3,42 @@
 
 #include <mysql-cppconn-8/mysqlx/xdevapi.h>
 
- 
-int main(int, char**) {
+using ::std::cout;
+using ::std::endl;
+using namespace ::mysqlx;
+
+int main(int argc, const char* argv[]){
     std::cout << "Hello, world!\n";
+    try
+    {
+        const char   *url = (argc > 1 ? argv[1] : "mysqlx://root@127.0.0.1");
+
+        cout << "Creating session on " << url
+            << " ..." << endl;
+
+        Session sess(url);
+
+        cout <<"Session accepted, creating collection..." <<endl;
+
+        Schema sch= sess.getSchema("test");
+        Collection coll= sch.createCollection("c1", true);
+
+        cout <<"Done!" <<endl;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        cout <<"ERROR: " <<err <<endl;
+        return 1;
+    }
+    catch (std::exception &ex)
+    {
+        cout <<"STD EXCEPTION: " <<ex.what() <<endl;
+        return 1;
+    }
+    catch (const char *ex)
+    {
+        cout <<"EXCEPTION: " <<ex <<endl;
+        return 1;
+    }
+
 } 
