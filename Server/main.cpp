317e4e91322517b1359fcaf785256ea8de96f4ea
fmol00 <fmol00@mail.ru>
2022-07-14 22:39:15 +0300

    Расшифровывает русские буквы из GET запроса

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,6 +1,7 @@
 #include <mysql-cppconn-8/mysqlx/xdevapi.h> //cpp Коннектор для mySQL
 #define _TURN_OFF_PLATFORM_STRING
 #include <cpprest/http_listener.h> 
+#include <cpprest/base_uri.h>
 
 using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
@@ -13,15 +14,19 @@ int main(int argc, const char* argv[]){
     
     int count = 0;
 
-    listener.support(methods::GET, [count] (http_request request) mutable
+    listener.support([count] (http_request request) mutable
     {
-        std::cout << "GET "<< request.request_uri().to_string() << std::endl;
+        //std::cout << "GET "<< request.request_uri().to_string() << std::endl;
 
 
-        // здесь я получаю параметры получения
+        // 
         auto http_get_vars = uri::split_query(request.request_uri().query());
+        http_get_vars.at("param1");
 
-        std::cout<< "http_get_vars="<<http_get_vars.at(0)<<std::endl;
+        std::cout<< "http_get_vars.at(\"param1\")="<<http_get_vars.at("param1")<<std::endl;
+        std::string str=web::uri::decode(http_get_vars.at("param1"));
+        std::cout<< "str="<<str<<std::endl;
+/*  
 
         // так анализируются параметры
         auto param_end = http_get_vars.find("end");
@@ -35,7 +40,7 @@ int main(int argc, const char* argv[]){
             auto request_name = param_start->second;
             std::cout << "Received \"start\": " << request_name << std::endl;
         }
-
+*/
         request.reply(status_codes::OK, utility::conversions::to_string_t("bodydata1"),utility::conversions::to_string_t("text/html"));
     }
     );
