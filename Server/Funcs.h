975ab8c8a542ef575354a21979bd5483dbeceba8
fmol00 <fmol00@mail.ru>
2022-07-14 18:09:13 +0300

    Закончить создание функции для выбора записи по id

--- Server/Funcs.h
+++ Server/Funcs.h
@@ -2,14 +2,16 @@
 #define FUNCS_H
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <utility>
 #include <vector>
+#include <string>
 
 #include <mysql-cppconn-8/mysqlx/xdevapi.h> //cpp Коннектор для mySQL
 
 //Возрващает вектор пар координат всех точек в регионе
-int GetCoordByTag(const std::string &DBName, const std::string &StrTag, std::vector<std::pair<double, double>> &Coord)
+int GetCoordByTag(const std::string &StrTag, std::vector<std::pair<double, double>> &Coord, const std::string &DBName)
 {
     using ::std::cout;
     using ::std::endl;
@@ -68,4 +70,74 @@ int GetCoordByTag(const std::string &DBName, const std::string &StrTag, std::vec
     return 0;
 }
 
+struct entity
+{
+    int id;
+    std::string name;
+    std::string tag;
+    std::string description;
+    double latitude;
+    double longitude;
+    std::string grouped_id;
+};
+
+void Print(const entity &Entity)
+{
+    std::cout << std::fixed;
+    std::cout << std::setprecision(8);
+    std::cout<<"id="<<Entity.id<<"; name="<<Entity.name<<"; tag="<<Entity.tag<<"; description="<<Entity.description<<"; latitude="<<Entity.latitude<<"; longitude="<<Entity.longitude<<std::endl; 
+}
+int GetEntity(const std::string &DBid, entity &Entity, const std::string &DBName)
+{  
+    using ::std::cout;
+    using ::std::endl;
+    using namespace ::mysqlx;
+    try
+    {
+        std::string StrTegTemp=DBid;
+        StrTegTemp.insert(0, "\"");
+        StrTegTemp.append("\"");
+        
+        const char* url = "mysqlx://root:qwertyz@127.0.0.1";
+
+        cout << "Creating session on " << url << " ..." << endl;
+        Session sess(url);
+        cout <<"Session accepted, getting"<< DBName << "schema ..." <<endl;
+        Schema sch= sess.getSchema(DBName);
+        cout <<"Schema got, getting Places Table ..." <<endl;
+        Table tlPlaces = sch.getTable("Places", true);
+        cout <<"Places Table got, getting entity ..." <<endl;
+
+        std::string strReq("id=");
+        strReq.append(StrTegTemp);
+        cout<<"strReq:"<<strReq<<endl;
+
+        RowResult rres = tlPlaces.select("*").where(strReq).execute();
+        Row row=rres.fetchOne();
+
+        Entity.id= row.get(0);
+        Entity.name = (std::string)row.get(1);
+        Entity.tag = (std::string)row.get(2);
+        Entity.description = (std::string)row.get(3);
+        Entity.latitude = row.get(4);
+        Entity.longitude = row.get(5);
+        Entity.grouped_id = (std::string)row.get(6);
+    }
+    catch (const mysqlx::Error &err)
+    {
+        cout <<"ERROR: " <<err <<endl;
+        return 1;
+    }
+    catch (std::exception &ex)
+    {
+        cout <<"STD EXCEPTION: " <<ex.what() <<endl;
+        return 1;
+    }
+    catch (const char *ex)
+    {
+        cout <<"EXCEPTION: " <<ex <<endl;
+        return 1;
+    }
+    return 0;
+}
 #endif /* FUNCS_H */
--- Server/main.cpp
+++ Server/main.cpp
@@ -13,8 +13,9 @@ using ::std::endl;
 using namespace ::mysqlx;
 */
 int main(int argc, const char* argv[]){
+/* 
     std::vector<std::pair<double, double>> vecRes;
-    GetCoordByTag("TouristPlaces1", "РеспубликаКрым", vecRes);
+    GetCoordByTag("РеспубликаКрым", vecRes, "TouristPlaces1");
     
     std::cout << std::fixed;
     std::cout << std::setprecision(8);
@@ -22,45 +23,14 @@ int main(int argc, const char* argv[]){
     {
         std::cout<<"lat="<<entity.first<<" long="<<entity.second<<std::endl;
     }
-    
+*/    
+    entity Entity;
+    GetEntity("3", Entity, "TouristPlaces1");
+    Print(Entity);
+
+
+
+
+
     return 0;
-
-
-
-/*    try
-    {
-        const char   *url = (argc > 1 ? argv[1] : "mysqlx://root:qwertyz@127.0.0.1");
-
-        cout << "Creating session on " << url
-            << " ..." << endl;
-
-        Session sess(url);
-
-        cout <<"Session accepted, creating collection..." <<endl;
-
-        Schema sch= sess.getSchema("TouristPlaces1");
-        Table tlPlaces = sch.getTable("Places", true);
-        RowResult rres = tlPlaces.select("*").execute();
-        std::string str=(std::string)rres.fetchOne().get(1);
-        cout<<"str="<<str<<endl;
-
-
-        cout <<"Done!" <<endl;
-    }
-    catch (const mysqlx::Error &err)
-    {
-        cout <<"ERROR: " <<err <<endl;
-        return 1;
-    }
-    catch (std::exception &ex)
-    {
-        cout <<"STD EXCEPTION: " <<ex.what() <<endl;
-        return 1;
-    }
-    catch (const char *ex)
-    {
-        cout <<"EXCEPTION: " <<ex <<endl;
-        return 1;
-    }
-*/
 } 
